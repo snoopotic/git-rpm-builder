14d7afaafbf68ef28a1fa46acf29a6f458d0fc86
Felix Herzog <snoopotic@gmail.com>
2019-09-01 13:04:18 +0200

    needing more build args

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ if [ ! "${GITVERSION}" ]; then
     export GITVERSION=$(curl --silent "https://api.github.com/repos/git/git/tags" | jq -r '.[0].name' | cut -d v -f 2);
 fi
 
-docker build --build-arg GITVERSION=${GITVERSION} -t uploadimage .
+docker build --build-arg GITVERSION=${GITVERSION} --build-arg BTUSER=${BTUSER} --build-arg BTKEY=${BTKEY} -t uploadimage .
